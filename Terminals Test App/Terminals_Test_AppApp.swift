1bf0a1fc613edd9e358684bc937663933d2fa84f
yemsuhari <yemsuhari@gmail.com>
2021-07-26 13:27:57 +0300

    Initial Commit

---
+++ Terminals Test App/Terminals_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Terminals_Test_AppApp.swift
+//  Terminals Test App
+//
+//  Created by Fyodor Vladimirov on 26.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct Terminals_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
