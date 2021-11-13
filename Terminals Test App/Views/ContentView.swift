b0c9374928eb7718b943411e627b5ba70b2acb0f
yemsuhari <albertpetrovich3@gmail.com>
2021-11-13 23:52:15 +0300

    - add toast popup for "Сохранить" button

--- Terminals Test App/Views/ContentView.swift
+++ Terminals Test App/Views/ContentView.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 import Foundation
 import CoreLocation
+import PopupView
 
 struct ContentView: View
 {
+    @State var showingPopup = false
     
     @EnvironmentObject var content: ContentModel
     
@@ -18,165 +20,190 @@ struct ContentView: View
     
     var body: some View
     {
-        NavigationView
+        ZStack
         {
-            VStack
+            NavigationView
             {
-                List
+                VStack
                 {
-                    NavigationLink(destination: SecondView())
+                    List
                     {
-                        // Откуда
-                        VStack
+                        NavigationLink(destination: SecondView())
                         {
-                            ZStack
+                            // Откуда
+                            VStack
                             {
-                                Rectangle()
-                                    .foregroundColor(Color("White"))
-                                HStack
+                                ZStack
                                 {
-                                    Text(self.content.searchTextOne)
-                                }
-                            }
-                            
-                            // Подробности
-                            ForEach(content.object!.city, id: \.id)
-                            { city in
-                                ForEach(city.terminals.terminal, id: \.id)
-                                { terminal in
-                                    if content.fromId == terminal.id
+                                    Rectangle()
+                                        .foregroundColor(Color("White"))
+                                    HStack
                                     {
-                                        Text(terminal.address)
-                                            .padding(.top)
-                                            .padding(.bottom)
-                
-                                        Text("Время для прибытия: ")
-                                            //.font(.system(size: 1000))
-                                            .scaledToFit()
-                                            .minimumScaleFactor(0.01)
-                                            .lineLimit(1)
-                                        Text(terminal.calcSchedule.arrival)
-                                            //.font(.system(size: 1000))
-                                            .scaledToFit()
-                                            .minimumScaleFactor(0.01)
-                                            .lineLimit(1)
-                                        
-                                        Text("Время для отбытия: ")
-                                            //.font(.system(size: 1000))
-                                            .scaledToFit()
-                                            .minimumScaleFactor(0.01)
-                                            .lineLimit(1)
-                                        Text(terminal.calcSchedule.derival)
-                                            //.font(.system(size: 1000))
-                                            .scaledToFit()
-                                            .minimumScaleFactor(0.01)
-                                            .lineLimit(1)
-                                        
-                                        
-                                        // Расстояние до терминалов
-                                        var distaceOne:String = content.findDistance(terminalLatitude: Double(terminal.latitude)!, terminalLongitude: Double(terminal.longitude)!)
-                                        
-                                        Image(systemName: "map")
-                                            .foregroundColor(.blue)
-                                            .imageScale(.large)
-                                            .padding(.top)
-                                        Text("Растояние до терминала: ")
-                                        Text("\(distaceOne) километров")
-                                        
-                                        
-                                        
+                                        Text(self.content.searchTextOne)
                                     }
                                 }
-                            }
-
-                            
-                        }
-                        
-                        
-                    }
-                    
-                    
-                    // Куда
-                    NavigationLink(destination: SecondView())
-                    {
-                        VStack
-                        {
-                            ZStack
-                            {
-                                Rectangle()
-                                    .foregroundColor(Color("White"))
-                                HStack
-                                {
-                                    Text(content.searchTextTwo)
-                                }
-                            }
-                            
-                            ForEach(content.object!.city, id: \.id)
-                            { city in
-                                ForEach(city.terminals.terminal, id: \.id)
-                                { terminal in
-                                    if content.toId == terminal.id
-                                    {
-                                        Text(terminal.address)
-                                            .padding(.top)
-                                            .padding(.bottom)
-                
-                                        Text("Время для прибытия: ")
-                                            //.font(.system(size: 1000))
-                                            .scaledToFit()
-                                            .minimumScaleFactor(0.01)
-                                            .lineLimit(1)
-                                        Text(terminal.calcSchedule.arrival)
-                                            //.font(.system(size: 1000))
-                                            .scaledToFit()
-                                            .minimumScaleFactor(0.01)
-                                            .lineLimit(1)
-                                        
-                                        Text("Время для отбытия: ")
-                                            //.font(.system(size: 1000))
-                                            .scaledToFit()
-                                            .minimumScaleFactor(0.01)
-                                            .lineLimit(1)
-                                        Text(terminal.calcSchedule.derival)
-                                            //.font(.system(size: 1000))
-                                            .scaledToFit()
-                                            .minimumScaleFactor(0.01)
-                                            .lineLimit(1)
-                                        
-                                        
-                                        // Расстояние до терминалов
-                                        var distaceTwo:String = content.findDistance(terminalLatitude: Double(terminal.latitude)!, terminalLongitude: Double(terminal.longitude)!)
-                                        
-                                        Image(systemName: "map")
-                                            .foregroundColor(.blue)
-                                            .imageScale(.large)
-                                            .padding(.top)
-                                        Text("Растояние до терминала: ")
-                                        Text("\(distaceTwo) километров")
-                                    }
-                                }
-                            }
-
-                        }
-                    }
-                    
-                                        
-                    
-                    // Кнопка "Сохранить"
-                    Button(action:  {}, label: {
-                        Text("Сохранить")
-                            .foregroundColor(.blue)
-                            .frame(maxWidth: .infinity, alignment: .center)
-                            .offset(x: -10)
-                            .padding()
-                    })
-                }
                                 
-                
+                                // Подробности
+                                ForEach(content.object!.city, id: \.id)
+                                { city in
+                                    ForEach(city.terminals.terminal, id: \.id)
+                                    { terminal in
+                                        if content.fromId == terminal.id
+                                        {
+                                            Text(terminal.address)
+                                                .padding(.top)
+                                                .padding(.bottom)
+                    
+                                            Text("Время для прибытия: ")
+                                                //.font(.system(size: 1000))
+                                                .scaledToFit()
+                                                .minimumScaleFactor(0.01)
+                                                .lineLimit(1)
+                                            Text(terminal.calcSchedule.arrival)
+                                                //.font(.system(size: 1000))
+                                                .scaledToFit()
+                                                .minimumScaleFactor(0.01)
+                                                .lineLimit(1)
+                                            
+                                            Text("Время для отбытия: ")
+                                                //.font(.system(size: 1000))
+                                                .scaledToFit()
+                                                .minimumScaleFactor(0.01)
+                                                .lineLimit(1)
+                                            Text(terminal.calcSchedule.derival)
+                                                //.font(.system(size: 1000))
+                                                .scaledToFit()
+                                                .minimumScaleFactor(0.01)
+                                                .lineLimit(1)
+                                            
+                                            
+                                            // Расстояние до терминалов
+                                            var distaceOne:String = content.findDistance(terminalLatitude: Double(terminal.latitude)!, terminalLongitude: Double(terminal.longitude)!)
+                                            
+                                            Image(systemName: "map")
+                                                .foregroundColor(.blue)
+                                                .imageScale(.large)
+                                                .padding(.top)
+                                            Text("Растояние до терминала: ")
+                                            Text("\(distaceOne) километров")
+                                            
+                                            
+                                            
+                                        }
+                                    }
+                                }
+
+                                
+                            }
+                            
+                            
+                        }
+                        
+                        
+                        // Куда
+                        NavigationLink(destination: SecondView())
+                        {
+                            VStack
+                            {
+                                ZStack
+                                {
+                                    Rectangle()
+                                        .foregroundColor(Color("White"))
+                                    HStack
+                                    {
+                                        Text(content.searchTextTwo)
+                                    }
+                                }
+                                
+                                ForEach(content.object!.city, id: \.id)
+                                { city in
+                                    ForEach(city.terminals.terminal, id: \.id)
+                                    { terminal in
+                                        if content.toId == terminal.id
+                                        {
+                                            Text(terminal.address)
+                                                .padding(.top)
+                                                .padding(.bottom)
+                    
+                                            Text("Время для прибытия: ")
+                                                //.font(.system(size: 1000))
+                                                .scaledToFit()
+                                                .minimumScaleFactor(0.01)
+                                                .lineLimit(1)
+                                            Text(terminal.calcSchedule.arrival)
+                                                //.font(.system(size: 1000))
+                                                .scaledToFit()
+                                                .minimumScaleFactor(0.01)
+                                                .lineLimit(1)
+                                            
+                                            Text("Время для отбытия: ")
+                                                //.font(.system(size: 1000))
+                                                .scaledToFit()
+                                                .minimumScaleFactor(0.01)
+                                                .lineLimit(1)
+                                            Text(terminal.calcSchedule.derival)
+                                                //.font(.system(size: 1000))
+                                                .scaledToFit()
+                                                .minimumScaleFactor(0.01)
+                                                .lineLimit(1)
+                                            
+                                            
+                                            // Расстояние до терминалов
+                                            var distaceTwo:String = content.findDistance(terminalLatitude: Double(terminal.latitude)!, terminalLongitude: Double(terminal.longitude)!)
+                                            
+                                            Image(systemName: "map")
+                                                .foregroundColor(.blue)
+                                                .imageScale(.large)
+                                                .padding(.top)
+                                            Text("Растояние до терминала: ")
+                                            Text("\(distaceTwo) километров")
+                                        }
+                                    }
+                                }
+
+                            }
+                        }
+                        
+                                            
+                        
+                        // Кнопка "Сохранить"
+                        Button(
+                            action:
+                            {
+                                self.showingPopup = true
+                            },
+                            label:
+                            {
+                                Text("Сохранить")
+                                    .foregroundColor(.blue)
+                                    .frame(maxWidth: .infinity, alignment: .center)
+                                    .offset(x: -10)
+                                    .padding()
+                            }
+                        )
+                            
+                    }
+                    
+                }
+                .navigationTitle("Терминалы")
             }
-            .navigationTitle("Терминалы")
+            .environmentObject(content)
         }
-        .environmentObject(content)
+        .popup(isPresented: $showingPopup, type: .toast, autohideIn: 2)
+        {
+            HStack
+            {
+                Text("Сохранено")
+                
+                Image (systemName: "checkmark")
+            }
+            .frame(width: 350, height: 39)
+            .background(Color.blue)
+            .foregroundColor(Color.white)
+            .cornerRadius(12)
+            .padding(.bottom, 30)
+        }
+
     }
 }
 
